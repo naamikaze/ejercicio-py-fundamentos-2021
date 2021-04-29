5cac4c5b99a3efbcc34d1bf127110860b228bc73
Nacho <oo>
2021-04-28 21:24:48 -0300

    Update ej8.py

--- tp3/ej8.py
+++ tp3/ej8.py
@@ -1,40 +1,31 @@
 cliente = int(input('Ingrese el nro de cliente: '))
 pagar= int(input('Ingrese el importe su factura: '))
-
 final = 0
-calculo = 0
-porc = 0
-var = 0
+descuento = 0
 
 
-#
 def veinte(importe): 
     final = importe
     return final
 
-
-#
 def diez(importe): 
-    var = importe*0.02
-    if var > 120:
+    descuento = importe*0.02
+    if descuento > 120:
         final = importe
         final -= 120
     else:
         final = importe-importe*0.02
 
     return final
-#
 
-#
 def treinta(importe): 
-    porc = importe*0.10
-    if porc > 150:
+    descuento = importe*0.10
+    if descuento > 150:
         final = importe 
         final += 150
     else:
-        final = importe+porc
+        final = importe+descuento
     return final
-#
 
 print(f'El cliente nro: {cliente} debera abonar lo siguiente:')
 print(f'Si el cliente paga antes de los primeros 10 días deberá abonar: {diez(pagar)}')
